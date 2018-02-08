540f913fd4f96a77069c143671d402ea66e39e47
macmorning <sylvain.yvon@gmail.com>
2018-02-08 08:27:42 +0100

    finished v1.0

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+const isChrome = (chrome !== undefined);
+
 const clock = {
     seconds: 0,
     onABreak: false,
@@ -5,111 +7,123 @@ const clock = {
     paused: false,
     streakTimer: 0,
     pauseTimer: 0,
-    ring:{},
-    start: function() {
+    ring: {},
+    start: function () {
         this.ticking = true;
         this.paused = false;
-        this.seconds = this.streakTimer*60;
+        this.seconds = this.streakTimer * 60;
         this.onABreak = false;
-        chrome.browserAction.setIcon({path: "icons/clock_red-48.png"});
-        if (typeof(Storage) !== "undefined") {
+        if (isChrome) {
+            chrome.browserAction.setIcon({path: "icons/clock_red-48.png"});
+        } else {
+            browser.browserAction.setIcon({path: "icons/clock_red-48.png"});
+        }
+        if (typeof (Storage) !== "undefined") {
             localStorage.streakTimer = this.streakTimer;
-            localStorage.pauseTimer= this.pauseTimer;
+            localStorage.pauseTimer = this.pauseTimer;
         }
     },
-    reset: function() {
+    reset: function () {
         this.ticking = false;
         this.paused = false;
-        this.seconds = this.streakTimer*60;
+        this.seconds = this.streakTimer * 60;
         this.onABreak = false;
-        chrome.browserAction.setIcon({path: "icons/clock-48.png"});
-        if (typeof(Storage) !== "undefined") {
+        if (isChrome) {
+            chrome.browserAction.setIcon({path: "icons/clock-48.png"});
+        } else {
+            browser.browserAction.setIcon({path: "icons/clock-48.png"});
+        }
+        if (typeof (Storage) !== "undefined") {
             localStorage.streakTimer = this.streakTimer;
-            localStorage.pauseTimer= this.pauseTimer;
+            localStorage.pauseTimer = this.pauseTimer;
         }
     },
-    pause: function() {
-      this.paused = !this.paused;
+    pause: function () {
+        this.paused = !this.paused;
     },
-    getCurrentState: function() {
+    getCurrentState: function () {
         return {
-            "seconds": this.seconds, 
-            "onABreak": this.onABreak, 
-            "ticking": this.ticking, 
+            "seconds": this.seconds,
+            "onABreak": this.onABreak,
+            "ticking": this.ticking,
             "paused": this.paused,
             "streakTimer": this.streakTimer,
             "pauseTimer": this.pauseTimer
         };
     },
-    tick: function() {
-        console.log("tick - " + this.seconds);
-        if(!this.ticking || this.paused) {
+    tick: function () {
+        if (!this.ticking || this.paused) {
             return true;
         }
-        if(this.seconds > 0) {
+        if (this.seconds > 0) {
             this.seconds--;
         } else {
             this.onABreak = !this.onABreak;
             try {
-            this.ring.play();
-            } catch(e) { console.log("could not ring: " + e )};
+                this.ring.play();
+            } catch (e) {
+                console.log("could not ring: " + e);
+            }
 
             let minutes = 0;
-            if(this.onABreak) {
-                chrome.browserAction.setIcon({path: "icons/clock_green-48.png"});
+            if (this.onABreak) {
+                if (isChrome) {
+                    chrome.browserAction.setIcon({path: "icons/clock_green-48.png"});
+                } else {
+                    browser.browserAction.setIcon({path: "icons/clock_green-48.png"});
+                }
                 minutes = this.pauseTimer;
             } else {
-                chrome.browserAction.setIcon({path: "icons/clock_red-48.png"});
+                if (isChrome) {
+                    chrome.browserAction.setIcon({path: "icons/clock_red-48.png"});
+                } else {
+                    browser.browserAction.setIcon({path: "icons/clock_red-48.png"});
+                }
                 minutes = this.streakTimer;
             }
             this.seconds = minutes * 60;
-            if(Notification.permission === "granted") {
+            try {
                 let text = (this.onABreak ? "Time for a " + minutes + " min break" : "Ready for a new " + minutes + " min streak?");
-                let notification = new Notification("Ding !", {
-                    icon: 'icons/clock-48.png',
-                    body: text
-                });
+                let notifDetail = {
+                    type: "basic",
+                    title: "Ding!",
+                    iconUrl: "icons/clock-48.png",
+                    message: text
+                };
+                if (isChrome) {
+                    chrome.notifications.create(notifDetail);
+                } else {
+                    browser.notifications.create(notifDetail);
+                }
+            } catch (e) {
+                console.log("could not display notification: " + e);
             }
         }
         return true;
     }
-};  
+};
 
-// If the browser allows it, ask for permissions to open a desktop notification
-if ("Notification" in window) {
-    if (Notification.permission !== "granted" && Notification.permission !== "denied") {
-        Notification.requestPermission();
-    }
-}
-    
 // If the browser allows it, use local storage to save set durations for the next session
-if (typeof(Storage) !== "undefined") {
-    if(parseInt(localStorage.streakTimer)) {
+if (typeof (Storage) !== "undefined") {
+    if (parseInt(localStorage.streakTimer)) {
         clock.streakTimer = parseInt(localStorage.streakTimer);
-        clock.seconds = clock.streakTimer*60;
+        clock.seconds = clock.streakTimer * 60;
     }
-    if(parseInt(localStorage.pauseTimer)) {
+    if (parseInt(localStorage.pauseTimer)) {
         clock.pauseTimer = parseInt(localStorage.pauseTimer);
     }
 }
 
 // Initialize and load the ring sound
-clock.ring = document.createElement('audio');
-clock.ring.setAttribute('src', 'sound/bell-ringing-01.mp3');
+clock.ring = document.createElement("audio");
+clock.ring.setAttribute("src", "sound/bell-ringing-01.mp3");
 
-setInterval(function() {
+setInterval(function () {
     clock.tick();
 }, 1000);
 
 // Configure message listener
-if(chrome !== undefined) {
-    console.log(chrome);
-    const browser = chrome;
-    console.log(browser.runtime);
-}
-
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+var msgListener = function (message, sender, sendResponse) {
     console.log("received message from content script: " + JSON.stringify(message));
     if (message.command === "getCurrentState") {
         console.log("sending response: " + JSON.stringify(clock.getCurrentState()));
@@ -124,8 +138,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         clock.pauseTimer = message.pauseTimer;
         clock.reset();
         sendResponse(true);
-   } else if (message.command === "pause") {
+    } else if (message.command === "pause") {
         clock.pause();
         sendResponse(true);
     }
-});
+};
+
+if (isChrome) {
+    chrome.runtime.onMessage.addListener(msgListener);
+} else {
+    browser.runtime.onMessage.addListener(msgListener);
+}
--- pomodoro.js
+++ pomodoro.js
@@ -5,84 +5,72 @@ const clock = {
     paused: false,
     streakTimer: 0,
     pauseTimer: 0,
-    pause: function() {
-      chrome.runtime.sendMessage({"command":"pause"},this.getCurrentState);
+    pause: function () {
+        chrome.runtime.sendMessage({"command": "pause"}, this.getCurrentState);
     },
-    reset: function() {
-      chrome.runtime.sendMessage({"command":"reset","streakTimer":this.streakTimer,"pauseTimer":this.pauseTimer},this.getCurrentState);
+    reset: function () {
+        chrome.runtime.sendMessage({"command": "reset", "streakTimer": this.streakTimer, "pauseTimer": this.pauseTimer}, this.getCurrentState);
     },
-    start: function() {
-      chrome.runtime.sendMessage({"command":"start","streakTimer":this.streakTimer,"pauseTimer":this.pauseTimer},this.getCurrentState);
+    start: function () {
+        chrome.runtime.sendMessage({"command": "start", "streakTimer": this.streakTimer, "pauseTimer": this.pauseTimer}, this.getCurrentState);
     },
-    getCurrentState: function() {
-      var responseHandler = function(response) {
-        clock.seconds = response.seconds;
-        clock.onABreak = response.onABreak;
-        clock.ticking = response.ticking;
-        clock.paused = response.paused;
-        clock.streakTimer = response.streakTimer;
-        clock.pauseTimer = response.pauseTimer;
-        clock.update();
-      }
-      chrome.runtime.sendMessage({"command":"getCurrentState"},responseHandler);
+    getCurrentState: function () {
+        var responseHandler = function (response) {
+            clock.seconds = response.seconds;
+            clock.onABreak = response.onABreak;
+            clock.ticking = response.ticking;
+            clock.paused = response.paused;
+            clock.streakTimer = response.streakTimer;
+            clock.pauseTimer = response.pauseTimer;
+            clock.update();
+        };
+        chrome.runtime.sendMessage({"command": "getCurrentState"}, responseHandler);
     },
-    update: function() {
-      document.querySelector("#clockHandSeconds").setAttribute('transform', 'rotate('+ 6*this.seconds +' 50 50)');
-      document.querySelector("#clockHandMinutes").setAttribute('transform', 'rotate('+ this.seconds/10 +' 50 50)');
-      document.querySelector("#streakTimer").value = this.streakTimer;
-      document.querySelector("#streakTimer").dispatchEvent(new Event("input"));
-      document.querySelector("#pauseTimer").value = this.pauseTimer;
-      document.querySelector("#pauseTimer").dispatchEvent(new Event("input"));
-      document.querySelector("#timeCounter").innerHTML = Math.floor(this.seconds/60).toString().paddingLeft("00") + ":" + Math.floor(this.seconds%60).toString().paddingLeft("00");
-      document.querySelector("#startBtn").innerHTML = (this.ticking ? "Reset" : "Start!");
+    update: function () {
+        document.querySelector("#clockHandSeconds").setAttribute("transform", "rotate(" + 6 * clock.seconds + " 50 50)");
+        document.querySelector("#clockHandMinutes").setAttribute("transform", "rotate(" + clock.seconds / 10 + " 50 50)");
+        document.querySelector("#streakTimer").value = clock.streakTimer;
+        document.querySelector("#streakTimer").dispatchEvent(new Event("input"));
+        document.querySelector("#pauseTimer").value = clock.pauseTimer;
+        document.querySelector("#pauseTimer").dispatchEvent(new Event("input"));
+        document.querySelector("#timeCounter").innerHTML = Math.floor(clock.seconds / 60).toString().padStart(2, "0") + ":" + Math.floor(clock.seconds % 60).toString().padStart(2, "0");
+        document.querySelector("#startBtn").innerHTML = (clock.ticking ? "Reset" : "Start!");
     },
-    tick: function() {
-      if(!this.ticking || this.paused) {
-        return true;
-      }
-      if(this.seconds > 0) {
-        this.seconds--;
-      } else {
-        if(this.onABreak) {
-          document.querySelector("#clockOnABreak").style.visibility = "visible";
-          minutes = document.querySelector("#pauseTimer").value;
-        } else {
-          document.querySelector("#clockOnABreak").style.visibility = "hidden";
-          minutes = document.querySelector("#streakTimer").value;        
+    tick: function () {
+        if (!this.ticking || this.paused) {
+            return true;
         }
-  
-        this.seconds = minutes * 60;
-      }
-      this.update();
-      return true;
+        if (this.seconds > 0) {
+            this.seconds--;
+        } else {
+            this.onABreak = !this.onABreak;
+            document.querySelector("#clockOnABreak").style.display = (clock.onABreak ? "block" : "none");
+            minutes = document.querySelector((clock.onABreak ? "#pauseTimer" : "#streakTimer")).value;
+            this.seconds = minutes * 60;
+        }
+        this.update();
+        return true;
     }
 };
-  
-  
-window.onload = function() {
 
-    // lets me use a padding function for strings
-    String.prototype.paddingLeft = function (paddingValue) {
-     return String(paddingValue + this).slice(-paddingValue.length);
-    };
-
+window.onload = function () {
     clock.getCurrentState();
 
-    document.querySelector("#streakTimer").oninput = function(evt) {
-      clock.streakTimer = evt.target.value;
-      document.querySelector("#streakTimer_value").innerHTML = evt.target.value;
+    document.querySelector("#streakTimer").oninput = function (evt) {
+        clock.streakTimer = evt.target.value;
+        document.querySelector("#streakTimer_value").innerHTML = evt.target.value;
     };
-    document.querySelector("#pauseTimer").oninput = function(evt) {
-      clock.pauseTimer = evt.target.value;
-      document.querySelector("#pauseTimer_value").innerHTML = evt.target.value;
+    document.querySelector("#pauseTimer").oninput = function (evt) {
+        clock.pauseTimer = evt.target.value;
+        document.querySelector("#pauseTimer_value").innerHTML = evt.target.value;
     };
 
     document.querySelector("#streakTimer").dispatchEvent(new Event("input"));
     document.querySelector("#pauseTimer").dispatchEvent(new Event("input"));
-    document.querySelector("#startBtn").onclick = function() { if(!clock.ticking) { clock.start(); } else { clock.reset();} };
-    document.querySelector("#container").onclick = function() { clock.pause(); };
+    document.querySelector("#startBtn").onclick = function () { if (!clock.ticking) { clock.start(); } else { clock.reset(); } };
+    document.querySelector("#container").onclick = function () { clock.pause(); };
 
-    setInterval(function() {
-      clock.tick();
+    setInterval(function () {
+        clock.tick();
     }, 1000);
-}
+};
